8c5065930a54a773133995febf0641f489e2e74c
Dave <dave.gagnat@gmail.com>
2019-10-18 01:38:01 -0500

    Updates

---
+++ precache-manifest.8f4733fdf12f315f3983aa60380dae6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7918b4944d1a75f6748c0eae0e8d1b95",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "859f23b03797e4877271",
+    "url": "/monsters-rolodex/static/css/main.b3c92c32.chunk.css"
+  },
+  {
+    "revision": "083fe96b724a22af0a1e",
+    "url": "/monsters-rolodex/static/js/2.d314b6ba.chunk.js"
+  },
+  {
+    "revision": "859f23b03797e4877271",
+    "url": "/monsters-rolodex/static/js/main.d24a913e.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
